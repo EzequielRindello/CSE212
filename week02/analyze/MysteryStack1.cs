0d859ff11cba0e853469085904c9c19547404677
Ezequiel Rindello <rindelloezequiel@gmail.com>
2024-06-20 18:10:20 -0300

    W02 Code: Stacks

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -1,13 +1,24 @@
-﻿public static class MysteryStack1 {
-    public static string Run(string text) {
+﻿public static class MysteryStack1
+{
+    public static string Run(string text)
+    {
+        // crea un stack 
         var stack = new Stack<char>();
+        // por cada letra en el texto recibido en la funcion se agrega al stack, la palabra queda igual.
+        // por ejemplo: "Hola" agrega cada letra: (H, o, l, a)
         foreach (var letter in text)
             stack.Push(letter);
 
+        // inicializa una variable para el resultado
         var result = "";
+        // este loop lo que hace es recorrer el largo de la texto y agregar el Pop() index a el resultado
+        // es decir: H o l a -> saca la a y lo agrega a result
+        //           H o l   -> saca la l y lo agrega a result
+        //           H o     -> saca la o y lo agrega a result
+        //           H       -> saca la H y lo agrega a result
         while (stack.Count > 0)
             result += stack.Pop();
 
-        return result;
+        return result; // como resultado obtenemos el texto invertido: aloH
     }
 }
--- week02/analyze/MysteryStack2.cs
+++ week02/analyze/MysteryStack2.cs
@@ -1,49 +1,71 @@
-﻿public static class MysteryStack2 {
-    private static bool IsFloat(string text) {
+﻿public static class MysteryStack2
+{
+    private static bool IsFloat(string text)
+    {// funcion que determina si una string puede ser pasado a float
         return float.TryParse(text, out _);
     }
 
-    public static float Run(string text) {
+    public static float Run(string text)
+    {
+        // crea un stack y lo guarda en una variable
         var stack = new Stack<float>();
-        foreach (var item in text.Split(' ')) {
-            if (item == "+" || item == "-" || item == "*" || item == "/") {
+        // la condicion del for lo que es hace es separar el texto recibido por argumento
+        // y convertirlo en un array, usa el parametro recibido, en este caso un espacio
+        // y separa en base a eso
+        // es decir: si el texto fuera "No entiendo nada" qudaria como
+        // ["No", "entiendo", "nada"]
+        foreach (var item in text.Split(' '))
+        {
+            // revisa cada parte del texto recibido con Or
+            if (item == "+" || item == "-" || item == "*" || item == "/")
+            {
+                // si el stack tiene menos de dos numeros para operar
                 if (stack.Count < 2)
                     throw new ApplicationException("Invalid Case 1!");
 
+                // crea dos variables y le asigna el valor del back del stack
                 var op2 = stack.Pop();
                 var op1 = stack.Pop();
+                // crea la variable que guarda el resultado de la operacion
                 float res;
-                if (item == "+") {
+                if (item == "+")
+                {
                     res = op1 + op2;
                 }
-                else if (item == "-") {
+                else if (item == "-")
+                {
                     res = op1 - op2;
                 }
-                else if (item == "*") {
+                else if (item == "*")
+                {
                     res = op1 * op2;
                 }
-                else {
+                else
+                {
                     if (op2 == 0)
                         throw new ApplicationException("Invalid Case 2!");
 
                     res = op1 / op2;
                 }
-
+                // guarda el resultado en el stack
                 stack.Push(res);
             }
-            else if (IsFloat(item)) {
+            else if (IsFloat(item)) // si no es una operacion simplemente lo guarda
+            {
                 stack.Push(float.Parse(item));
             }
-            else if (item == "") {
+            else if (item == "")// si esta vacio simplemente no hace nada
+            {
             }
-            else {
+            else
+            {//maneja inputs no validos
                 throw new ApplicationException("Invalid Case 3!");
             }
         }
 
-        if (stack.Count != 1)
+        if (stack.Count != 1)// verifica que quede solo un numero como resultado final
             throw new ApplicationException("Invalid Case 4!");
 
-        return stack.Pop();
+        return stack.Pop();// retorna el stack
     }
 }
